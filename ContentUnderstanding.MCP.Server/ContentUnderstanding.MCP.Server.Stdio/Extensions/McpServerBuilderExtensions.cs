fb7b80e9594bb5837212806b6b8ba48a56c8d164
Jason Specland <jaspecla@microsoft.com>
2025-05-06 11:39:00 -0400

    created initial project shell

---
+++ ContentUnderstanding.MCP.Server/ContentUnderstanding.MCP.Server.Stdio/Extensions/McpServerBuilderExtensions.cs
@@ -0,0 +1,34 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.SemanticKernel;
+using ModelContextProtocol.Server;
+
+namespace ContentUnderstanding.MCP.Server.Stdio.Extensions;
+
+/// <summary>
+/// Extension methods for <see cref="IMcpServerBuilder"/>.
+/// </summary>
+public static class McpServerBuilderExtensions
+{
+  /// <summary>
+  /// Adds all functions of the kernel plugins as tools to the server.
+  /// </summary>
+  /// <param name="builder">The builder instance.</param>
+  /// <param name="plugins">The kernel plugins to add as tools to the server.</param>
+  /// <returns>The builder instance.</returns>
+  public static IMcpServerBuilder WithTools(this IMcpServerBuilder builder, KernelPluginCollection plugins)
+  {
+    foreach (var plugin in plugins)
+    {
+      foreach (var function in plugin)
+      {
+#pragma warning disable SKEXP0001 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
+        builder.Services.AddSingleton(services => McpServerTool.Create(function.AsAIFunction()));
+#pragma warning restore SKEXP0001 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
+      }
+    }
+
+    return builder;
+  }
+}
--- ContentUnderstanding.MCP.Server/ContentUnderstanding.MCP.Server.Stdio/Program.cs
+++ ContentUnderstanding.MCP.Server/ContentUnderstanding.MCP.Server.Stdio/Program.cs
@@ -1,2 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.SemanticKernel;
+using System.Net.Http.Headers;
+using ContentUnderstanding.MCP.Server.Stdio.Extensions;
+using ContentUnderstanding.MCP.Tools;
+
+var builder = Host.CreateEmptyApplicationBuilder(settings: null);
+
+builder.Configuration
+  .AddJsonFile("appsettings.json", optional: true)
+  .AddUserSecrets<Program>()
+  .AddEnvironmentVariables();
+
+builder.Services.AddLogging(configure =>
+  configure
+    .AddConfiguration(builder.Configuration.GetSection("Logging"))
+    .AddFile(o => o.RootPath = AppContext.BaseDirectory)
+    .SetMinimumLevel(LogLevel.Debug)
+);
+
+builder.Services.AddKernel();
+
+builder.Services.AddSingleton(sp => KernelPluginFactory.CreateFromType<DummyTool>(serviceProvider: sp));
+
+var apiKey = builder.Configuration["Ynab:ApiKey"];
+
+builder.Services.AddHttpClient();
+
+var kernel = builder.Services.BuildServiceProvider().GetRequiredService<Kernel>();
+
+builder.Services
+    .AddMcpServer()
+    .WithStdioServerTransport()
+    .WithTools(kernel.Plugins);
+
+var app = builder.Build();
+
+var logger = app.Services.GetRequiredService<ILogger<Program>>();
+
+logger.LogInformation("Starting Content Understanding MCP Server");
+await app.RunAsync();
+logger.LogInformation("Shutting down Content Understanding MCP Server");
